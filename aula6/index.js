f602dbefa0efac49063b774c055546ea0e6fe631
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-11 20:20:33 -0300

    Variáveis com let

---
+++ aula6/index.js
@@ -0,0 +1,23 @@
+/* 
+Exemplo de palavras reservadas em JavaScript
+let function = "olá"; // "function" é uma palavra reservada e não pode ser usada como um nome de variável
+let if = 10; // "if" também é uma palavra reservada e não pode ser usada como um nome de variável
+*/
+
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos  camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+let nome = 'João'; // Inicializando a variável
+let nome2; // Declarou a variável
+
+console.log("Olá,", nome);
+console.log("Tudo bem,", nome, '?');
+console.log("O que você fez hoje,", nome, '?');
+console.log("Legal,", nome, '!', "E o que você mais gosta de fazer?");
+console.log("Que legal", nome, '!', 'E o que você quer ser quando crescer?');
+
