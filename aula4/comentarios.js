6e3397d1b353f901c20547d33f87aff4489b557a
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-11 18:06:18 -0300

    Código criado para exemplificar os comentário no js

---
+++ aula4/comentarios.js
@@ -0,0 +1,13 @@
+// Esse é um comentário em JavaScript
+console.log('Isso não é um comentário'); // Aqui tem outro comentário
+console.log('Linha 1');
+//console.log('Linha 2');
+// Mais um comentário :-)
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+
+/* 
+Um bloco de código 
+Linha de comentário.
+*/
