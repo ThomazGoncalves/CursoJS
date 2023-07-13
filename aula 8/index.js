fd460e396c24c324efcfc0ac9fc23c562ad6c89b
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-13 13:31:44 -0300

    mais exercícios

---
+++ aula 8/index.js
@@ -0,0 +1,17 @@
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+let imc = peso / (altura * altura);
+let dataAtual = new Date();
+let anoAtual = dataAtual.getFullYear();
+let anoNascimento = anoAtual - idade
+
+console.log(nome, sobrenome, 'tem', idade, 'anos,', 'pesa', peso, 'kg');
+
+// Template strings '${variável}'
+
+console.log(`tem ${altura}m de altura e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
+
