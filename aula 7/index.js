4b995ca0ee400fdb308537d56da151ac79a1b86a
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-13 12:22:25 -0300

    constantes com const

---
+++ aula 7/index.js
@@ -0,0 +1,27 @@
+// Constantes precisam ter nomes significativos
+// Não utilize palavras reservadas
+// Não pode começar o nome de uma constante com número
+// Não podem conter espaços ou traços
+// Utilizamos  camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+// String = Text | Number = Número
+
+const primeiroNumero = 10; // Number
+const segundoNumero = 2;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado += 5;
+
+console.log(resultadoTriplicado);
+
+console.log(typeof(primeiroNumero)); // O operador typeof retorna uma string representando o tipo de dado.
+
+
+
+
+
+
