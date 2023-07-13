70d0f2b87d2c0dbc9e0e2fa18d728710ec956582
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-13 13:43:15 -0300

    Let vs Var - primeira diferença

---
+++ aula9/index.js
@@ -0,0 +1,6 @@
+// ECMAScript - Padronização da linguagem  (ES6) JavScript criado por um empresa chamada ECMA
+var nome = 'Luiz';
+var nome = 'Otávio'; // Essa má prática de redeclarar as variáveis foi alterada com a feature let que chegou no ES6 em 2015
+
+nome = 'Thomaz'; // NÃO FAÇA ISSO, ESPECIFIQUE SEMPRE LET OU CONST, você cria uma variável global fazendo isso, o que pode atrapalhar o funcionamento da sua aplicação
+
