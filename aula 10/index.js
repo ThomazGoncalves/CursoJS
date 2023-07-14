1a9d57057a899d40137fc70f8ffc0648fb734b1d
Thomaz Gonçalves <thomazass100@gmail.com>
2023-07-14 08:35:03 -0300

    Tipo de dados primitivos

---
+++ aula 10/index.js
@@ -0,0 +1,11 @@
+// String, number, undefined, null, boolean, symbol
+
+const nome = 'Luiza'; // string
+const nome1 = "Luiza"; // string
+const nome2 = `Luiza`; // string
+const num1 = 10; // number
+const num2 = 10.5; // number
+let nomeAluno; // undefined = não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+const aprovado = true; // Boolean = true, false (lógico)
+console.log(typeof nome, nome)
