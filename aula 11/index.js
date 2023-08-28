42131f03915af239146aed91c70b0ee598d5dcc6
Thomaz Gonçalves <thomazass100@gmail.com>
2023-08-28 16:30:01 -0300

    Create index.js

---
+++ aula 11/index.js
@@ -0,0 +1,41 @@
+/*
+
+* Aritméticos
+* + Adição / Concatenação (- / *)
+* ** Potenciação 
+* % Resto da divisão 
+
+Precedência de operadores
+()
+**
+* / %
++ - 
+
+Incrimento = ++
+Decremento = --
+
+Nan - Not a number
+
+usamos para converter para inteiro: parseInt('2')
+usamos para converter para decimal: parseFloat('5.2')
+usamos number para converter automaticamente, o js vai encontrar a melhor opção: Number()
+
+*/
+
+let numero = 5;
+numero++;
+numero++;
+numero++; // incrementa o valor de "numero" em 1
+console.log(numero); // imprime 8
+
+
+let num1 = 2;
+
+console.log(num1++);
+
+
+
+let numero2 = 10
+console.log(--numero2);
+
+
