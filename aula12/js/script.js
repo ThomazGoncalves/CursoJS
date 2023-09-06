7366459d5c7e4807c2699a8e16d3e7baf5c46134
Thomaz Gonçalves <thomazass100@gmail.com>
2023-09-05 22:46:41 -0300

    Novas atividades: Alert, Confirm, Prompt

---
+++ aula12/js/script.js
@@ -0,0 +1,16 @@
+// Métodos do objeto window
+
+
+// alert('Com a nossa mensagem.');
+
+// confirm('Deseja realmente apagar?');
+
+// prompt('Digite seu nome'); 
+
+// const confirmar = confirm('Você tem certeza que deseja enviar?');
+
+const num1 = Number(prompt('Digite um número'));
+const num2 = Number(prompt('Digite outro número'));
+
+alert(`num1 + num2 = ${num1+num2}`);
+
